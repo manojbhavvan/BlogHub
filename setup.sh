92d4bad95d652ddcb8c030de2183a5c13b8084e8
Manoj Bhavvan <79741733+manojbhavvan@users.noreply.github.com>
2023-11-12 22:28:33 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,27 @@
 # setup.sh
-cd backend
-npm i
-cd..
-cd frontend
-npm i
-cd..
+#!/bin/bash
+
+# Navigate to the backend directory
+cd backend || exit 1
+
+# Run commands related to the backend setup
+npm install
+# Add your backend setup commands here
+
+# Move back to the original directory
+cd ..
+
+# Navigate to the frontend directory
+cd frontend || exit 1
+
+# Run commands related to the frontend setup
+npm install
+# Add your frontend setup commands here
+
+# Move back to the original directory
+cd ..
+
+# Additional setup commands if needed
+
+# Exit successfully
+exit 0
